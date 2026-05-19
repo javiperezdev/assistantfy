d3d1b8f15f19c7d871c0155a88de194317aa9ee8
javiperezdev <javiperezdev@gmail.com>
2026-05-19 16:19:58 +0200

    Improved context_manager efficiency

--- app/services/context_manager.py
+++ app/services/context_manager.py
@@ -3,20 +3,14 @@ from app.redis_client import redis_client
 
 EXPIRATION_TIME = 86400
 
-async def save_context(phone_number: str, data: dict):
+async def add_to_context(phone_number: str, message: dict):
     key = f"context:{phone_number}"
-    text_data = json.dumps(data)
-    await redis_client.set(key, text_data, ex=EXPIRATION_TIME)
+    await redis_client.rpush(key, json.dumps(message))
+    await redis_client.expire(key, EXPIRATION_TIME)
 
 async def get_context(phone_number: str):
     key = f"context:{phone_number}"
-    
-    text_data = await redis_client.get(key)
-    
+    text_data = await redis_client.lrange(key, 0, -1)
     if text_data:
-        return json.loads(text_data)
-
+        return [json.loads(item) for item in text_data]
     return []
-
-async def delete_context(phone_number: str):
-    await redis_client.delete(f"context:{phone_number}")
